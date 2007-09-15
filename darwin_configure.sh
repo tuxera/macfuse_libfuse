d428d84d3f7ff92ec1be92a090ff9d4e857517ad
Amit Singh <asingh@hidden>
2007-09-15 19:36:09 +0000

    Added darwin_configure.sh
    git-svn-id: http://macfuse.googlecode.com/svn/trunk@475 febdc8c4-1326-0410-abd6-27cb15717650

--- darwin_configure.sh
+++ darwin_configure.sh
@@ -1,1 +1,3 @@
+#! /bin/sh
+
 CFLAGS="-D__FreeBSD__=10 -D_POSIX_C_SOURCE=200112L -O -g -arch i386 -arch ppc -isysroot /Developer/SDKs/MacOSX10.5.sdk" LDFLAGS="-arch i386 -arch ppc -framework CoreFoundation" ./configure --prefix=/usr/local --disable-dependency-tracking
