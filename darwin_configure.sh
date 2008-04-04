583afb476f30f5405b89a13c9b2a8c358f1a2812
Amit Singh <asingh@hidden>
2008-04-04 00:02:13 +0000

    10.4: Added -mmacosx-version-min=10.4 to library CFLAGS so that it will build the 10.4 version on 10.5.
    git-svn-id: http://macfuse.googlecode.com/svn/trunk@812 febdc8c4-1326-0410-abd6-27cb15717650

--- darwin_configure.sh
+++ darwin_configure.sh
@@ -3,4 +3,4 @@
 MACFUSE_SRCROOT=${MACFUSE_SRCROOT:-$1}
 MACFUSE_SRCROOT=${MACFUSE_SRCROOT:?}
 
-CFLAGS="-D__FreeBSD__=10 -D_POSIX_C_SOURCE=200112L -I$MACFUSE_SRCROOT/common -O -g -arch i386 -arch ppc -isysroot /Developer/SDKs/MacOSX10.4u.sdk" LDFLAGS="-arch i386 -arch ppc -framework CoreFoundation" ./configure --prefix=/usr/local --disable-dependency-tracking
+CFLAGS="-D__FreeBSD__=10 -D_POSIX_C_SOURCE=200112L -I$MACFUSE_SRCROOT/common -O -g -arch i386 -arch ppc -isysroot /Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.4" LDFLAGS="-arch i386 -arch ppc -framework CoreFoundation" ./configure --prefix=/usr/local --disable-dependency-tracking
