88c50a1a677b0c25985c5491095018a3aab0b5a4
Ted Bonkenburg <inaddrany@hidden>
2008-10-10 00:25:36 +0000

    * Build library with x86_64 and ppc64 support.
    git-svn-id: http://macfuse.googlecode.com/svn/trunk@1183 febdc8c4-1326-0410-abd6-27cb15717650

--- darwin_configure.sh
+++ darwin_configure.sh
@@ -3,4 +3,4 @@
 MACFUSE_SRCROOT=${MACFUSE_SRCROOT:-$1}
 MACFUSE_SRCROOT=${MACFUSE_SRCROOT:?}
 
-CFLAGS="-D__FreeBSD__=10 -D_POSIX_C_SOURCE=200112L -I$MACFUSE_SRCROOT/common -O -g -arch i386 -arch ppc -isysroot /Developer/SDKs/MacOSX10.5.sdk" LDFLAGS="-arch i386 -arch ppc -framework CoreFoundation" ./configure --prefix=/usr/local --disable-dependency-tracking
+CFLAGS="-D__FreeBSD__=10 -D_POSIX_C_SOURCE=200112L -I$MACFUSE_SRCROOT/common -O -g -arch i386 -arch ppc -arch x86_64 -arch ppc64 -isysroot /Developer/SDKs/MacOSX10.5.sdk" LDFLAGS="-arch i386 -arch ppc -arch x86_64 -arch ppc64 -framework CoreFoundation" ./configure --prefix=/usr/local --disable-dependency-tracking
