ae5105bb1936fc03c9d92216e5cbf1b12cf0535d
Amit Singh <asingh@hidden>
2008-10-28 02:26:34 +0000

    Enforce gcc-4.0 for Tiger build.
    git-svn-id: http://macfuse.googlecode.com/svn/trunk@1283 febdc8c4-1326-0410-abd6-27cb15717650

--- darwin_configure.sh
+++ darwin_configure.sh
@@ -3,4 +3,4 @@
 MACFUSE_SRCROOT=${MACFUSE_SRCROOT:-$1}
 MACFUSE_SRCROOT=${MACFUSE_SRCROOT:?}
 
-CFLAGS="-D__FreeBSD__=10 -D_POSIX_C_SOURCE=200112L -I$MACFUSE_SRCROOT/common -O -g -arch i386 -arch ppc -arch x86_64 -arch ppc64 -isysroot /Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.4" LDFLAGS="-arch i386 -arch ppc -arch x86_64 -arch ppc64 -Wl,-no_uuid -framework CoreFoundation" ./configure --prefix=/usr/local --disable-dependency-tracking
+CC=gcc-4.0 CFLAGS="-D__FreeBSD__=10 -D_POSIX_C_SOURCE=200112L -I$MACFUSE_SRCROOT/common -O -g -arch i386 -arch ppc -arch x86_64 -arch ppc64 -isysroot /Developer/SDKs/MacOSX10.4u.sdk -mmacosx-version-min=10.4" LDFLAGS="-arch i386 -arch ppc -arch x86_64 -arch ppc64 -Wl,-no_uuid -framework CoreFoundation" ./configure --prefix=/usr/local --disable-dependency-tracking
