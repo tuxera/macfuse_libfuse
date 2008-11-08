b40115b00d13ff1461e802dccd7425ac77e66244
Amit Singh <asingh@hidden>
2008-11-08 12:02:11 +0000

    Added alternate configure helper script.
    git-svn-id: http://macfuse.googlecode.com/svn/trunk@1372 febdc8c4-1326-0410-abd6-27cb15717650

---
+++ darwin_configure_ino64.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+MACFUSE_SRCROOT=${MACFUSE_SRCROOT:-$1}
+MACFUSE_SRCROOT=${MACFUSE_SRCROOT:?}
+
+CFLAGS="-D__DARWIN_64_BIT_INO_T=1 -D__FreeBSD__=10 -D_POSIX_C_SOURCE=200112L -I$MACFUSE_SRCROOT/common -O -g -arch i386 -arch ppc -arch x86_64 -arch ppc64 -isysroot /Developer/SDKs/MacOSX10.5.sdk" LDFLAGS="-arch i386 -arch ppc -arch x86_64 -arch ppc64 -framework CoreFoundation" ./configure --prefix=/usr/local --disable-dependency-tracking --disable-static
